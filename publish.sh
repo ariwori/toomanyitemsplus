6056be1c03e71bd5d5134e0133e7e907959c7481
wqlin <i@wqlin.com>
2020-01-11 20:02:21 +0800

    以后更新压缩下代码再发布，MOD体积越来越大了，虽然是图片背锅

---
+++ publish.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+srcpath="src"
+
+function ergodic(){
+  for file in ` ls $1 `
+  do
+    if [ -d $1"/"$file ]
+    then
+      mkdir -pv "dist/"$1"/"$file
+      ergodic $1"/"$file
+    else
+      #echo "$1/$file"
+      if [[ $(echo $file | cut -d '.' -f2) == "lua" ]]
+      then
+        echo "Minify lua file: "$1"/"$file" to dist/"$1"/"$file
+        luamin -f $1"/"$file > "dist/"$1"/"$file
+      else
+        cp -v $1"/"$file "dist/"$1"/"$file
+      fi
+    fi
+  done
+}
+
+rm -rf dist/*
+cp LICENSE dist/
+ergodic $srcpath
+mv -v dist/$srcpath/* dist/
+rm -rf dist/$srcpath
